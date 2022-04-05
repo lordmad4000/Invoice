769d8bf165112cee53071d280f05b45bab4aa528
joseantonio <joseantonio@qcompusoft.com>
2022-04-05 11:01:23 +0200

    Agregado el repositorio de User.

---
+++ src/Users.Infra/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Users.Domain.Entities;
+using Users.Domain.Interfaces;
+using Users.Infra.Data;
+
+namespace Users.Infra.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(EFContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
