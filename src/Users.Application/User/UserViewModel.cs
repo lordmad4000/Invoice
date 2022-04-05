0bf656f0955bea2a09be0c97142a5245e66eddb6
joseantonio <joseantonio@qcompusoft.com>
2022-04-05 11:09:32 +0200

    Agregado el ViewModel de User.

---
+++ src/Users.Application/User/UserViewModel.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Users.Application.User
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "UserName is required.")]
+        [StringLength(20, ErrorMessage = "UserName cannot be longer than 20 characters.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 10 characters.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "FirstName is required.")]
+        [StringLength(20, ErrorMessage = "FirstName cannot be longer than 20 characters.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "LastName is required.")]
+        [StringLength(20, ErrorMessage = "LastName cannot be longer than 20 characters.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [StringLength(40, ErrorMessage = "Email cannot be longer than 40 characters.")]
+        public string Email { get; set; }
+        
+    }
+}
