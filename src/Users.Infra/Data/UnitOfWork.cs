125f1bf6a66c751d93c14b565e9b9c505e2c72ba
joseantonio <joseantonio@qcompusoft.com>
2022-04-06 10:22:57 +0200

    Agregado UnitOfWork a Infra.

---
+++ src/Users.Infra/Data/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Users.Domain.Base;
+using Users.Domain.Interfaces;
+using Users.Infra.Repositories;
+
+namespace Users.Infra.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly EFContext _dbContext;
+
+        public UnitOfWork(EFContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IAsyncRepository<T> AsyncRepository<T>() where T : BaseEntity
+        {
+            return new RepositoryBase<T>(_dbContext);
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return _dbContext.SaveChangesAsync();
+        }
+    }
+}
