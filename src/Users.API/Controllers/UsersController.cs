5d2fc146d97d229abf273c193b1c5e7b6655ebe3
joseantonio <joseantonio@qcompusoft.com>
2022-04-06 09:04:10 +0200

    Agregado el proyecto Users.API.

---
+++ src/Users.API/Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult> GetUsers()
+        {
+            ActionResult result = new OkResult();
+            await Task.Run(() =>
+            {
+                result = Ok();
+            });
+            return result;
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetUser(int id)
+        {
+            ActionResult result = new OkResult();
+            await Task.Run(() =>
+            {
+                result = Ok();
+            });
+            return result;
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser()
+        {
+            ActionResult result = new OkResult();
+            await Task.Run(() =>
+            {
+                result = Ok();
+            });
+            return result;
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult> PostUser()
+        {
+            ActionResult result = new OkResult();
+            await Task.Run(() =>
+            {
+                result = Ok();
+            });
+            return result;
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(int id)
+        {
+            ActionResult result = new OkResult();
+            await Task.Run(() =>
+            {
+                result = Ok();
+            });
+            return result;
+        }
+    }
+}
