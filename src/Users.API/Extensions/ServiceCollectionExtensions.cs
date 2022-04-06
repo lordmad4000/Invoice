d9fec4b4dad05a5df5a3ed98e8e06f0a9dba1d80
joseantonio <joseantonio@qcompusoft.com>
2022-04-06 10:23:27 +0200

    Agregados servicios de DI en API.

---
+++ src/Users.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Users.Infra.Repositories;
+using Users.Domain.Interfaces;
+using Users.Infra.Data;
+using Users.Application.Services;
+
+namespace Users.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddUnitOfWork(this IServiceCollection services)
+        {
+            return services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.AddDbContext<EFContext>(options => options.UseMySql(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services)
+        {
+            return services.AddScoped<UserService>();
+        }
+    }
+}
