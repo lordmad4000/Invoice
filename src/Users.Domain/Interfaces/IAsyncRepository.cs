f3c4f4c8320ca69e2f049273d76e9aaa49372ee3
joseantonio <joseantonio@qcompusoft.com>
2022-04-12 09:24:07 +0200

    Agregado sobrecarga a DeleteAsync y agregado el parametro tracking a GetAsync en IAsyncRepository.

--- src/Users.Domain/Interfaces/IAsyncRepository.cs
+++ src/Users.Domain/Interfaces/IAsyncRepository.cs
@@ -9,13 +9,10 @@ namespace Users.Domain.Interfaces
     public interface IAsyncRepository<T> where T : BaseEntity
     {
         Task<T> AddAsync(T entity);
-
         Task<T> UpdateAsync(T entity);
-
+        Task<bool> DeleteAsync(Guid id);
         Task<bool> DeleteAsync(T entity);
-
-        Task<T> GetAsync(Expression<Func<T, bool>> expression);
-
+        Task<T> GetAsync(Expression<Func<T, bool>> expression, bool tracking = true);
         Task<List<T>> ListAsync(Expression<Func<T, bool>> expression);
     }
 }
