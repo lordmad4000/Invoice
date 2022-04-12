693efdaebaf92bc2264e51cdf4bf65f6aaf02485
joseantonio <joseantonio@qcompusoft.com>
2022-04-12 09:18:15 +0200

    Agregadas las propiedades Active y ActivationCode a User.

--- src/Users.Domain/Entities/User/User.cs
+++ src/Users.Domain/Entities/User/User.cs
@@ -1,16 +1,17 @@
-using System;
 using Users.Domain.Base;
 using Users.Domain.ValueObjects;
 
 namespace Users.Domain.Entities
 {
-    public partial class User : BaseEntity<Guid>
+    public partial class User : BaseEntity
     {
         public string UserName { get; set; }
         public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string EmailAddress { get; set; }
+        public EmailAddress EmailAddress { get; set; }
+        public bool Active { get; set; }
+        public string ActivationCode { get; set; }
         
     }
 }
