1418caea1251fe6cd3253dafdab37384ea179260
joseantonio <joseantonio@qcompusoft.com>
2022-04-20 14:04:41 +0200

    Cambiado a privado los set de User.

--- src/Users.Domain/Entities/User/User.cs
+++ src/Users.Domain/Entities/User/User.cs
@@ -5,13 +5,13 @@ namespace Users.Domain.Entities
 {
     public partial class User : BaseEntity
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public EmailAddress EmailAddress { get; set; }
-        public bool Active { get; set; }
-        public string ActivationCode { get; set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public EmailAddress EmailAddress { get; private set; }
+        public bool Active { get; private set; }
+        public string ActivationCode { get; private set; }
         
     }
 }
