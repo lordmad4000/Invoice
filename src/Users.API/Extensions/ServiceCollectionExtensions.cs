a184109da0a56a6955ccf5bc4b35d60d086c24ba
joseantonio <joseantonio@qcompusoft.com>
2022-04-20 14:10:41 +0200

    Agregada extension para la inicialización de Swagger.

--- src/Users.API/Extensions/ServiceCollectionExtensions.cs
+++ src/Users.API/Extensions/ServiceCollectionExtensions.cs
@@ -6,6 +6,7 @@ using Users.Domain.Interfaces;
 using Users.Infra.Data;
 using Users.Application.Services;
 using Users.Application.Interfaces;
+using Microsoft.OpenApi.Models;
 
 namespace Users.API.Extensions
 {
@@ -31,7 +32,42 @@ namespace Users.API.Extensions
 
         public static IServiceCollection AddBusinessServices(this IServiceCollection services)
         {
-            return services.AddScoped<IUserService, UserService>();
+            services.AddScoped<INotificationService, NotificationHTMLService>();
+            services.AddScoped<IUserService, UserService>();
+            return services;
         }
+
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserWebAPI", Description = "Users Core API", Version = "v1" });
+                c.AddSecurityDefinition("basic", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "basic",
+                    In = ParameterLocation.Header,
+                    Description = "Basic Authorization header using the Bearer scheme."
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "basic"
+                                }
+                            },
+                            new string[] {}
+                    }
+                });
+            });
+            
+            return services;
+        }
+
     }
 }
