726e78bafaf0e1adb6d28d0dd101f1d06d320f9a
joseantonio <joseantonio@qcompusoft.com>
2022-04-27 10:23:48 +0200

    Agregado test de integracion a UserController.

---
+++ test/TestUsers.API/Integration/UserControllerTest.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using TestUsers.API.Mock;
+using Users.Application.Interfaces;
+using Users.Application.Services;
+using Users.Controllers;
+using Users.Domain.Interfaces;
+using Xunit;
+
+namespace TestUsers.API
+{
+    public class UserControllerTest
+    {
+
+        [Fact]
+        public async Task PostUser_Then_GetUsers_Then_GetUser__Then_UpdateUser_Then_DeleteUser()
+        {
+            IServiceCollection services = BuildDependencies();
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                var userController = serviceProvider.GetRequiredService<UserController>();
+
+                var testUser = new UserViewModel
+                {
+                    Id = Guid.Empty,
+                    UserName = "pepe",
+                    Password = "12345678",
+                    FirstName = "Pepe",
+                    LastName = "Luis",
+                    Email = "pepeluis@rmail.com",
+                };
+                string updatedPassword = "password";
+
+                var actionResult = await userController.PostUser(testUser);
+
+                var okObjectResult = Assert.IsType<OkObjectResult>(actionResult);
+                var user = Assert.IsType<UserViewModel>(okObjectResult.Value);
+
+                actionResult = await userController.GetUsers();
+
+                okObjectResult = Assert.IsType<OkObjectResult>(actionResult);
+                var users = Assert.IsType<List<UserViewModel>>(okObjectResult.Value);
+                Assert.NotEmpty(users);
+
+                actionResult = await userController.GetUser(user.Id);
+
+                okObjectResult = Assert.IsType<OkObjectResult>(actionResult);
+                user = Assert.IsType<UserViewModel>(okObjectResult.Value);
+                Assert.Equal(testUser.UserName, user.UserName);
+                Assert.Equal(testUser.Password, user.Password);
+                Assert.Equal(testUser.FirstName, user.FirstName);
+                Assert.Equal(testUser.LastName, user.LastName);
+                Assert.Equal(testUser.Email, user.Email);
+
+                user.Password = updatedPassword;
+                actionResult = await userController.PutUser(user);
+
+                okObjectResult = Assert.IsType<OkObjectResult>(actionResult);
+                user = Assert.IsType<UserViewModel>(okObjectResult.Value);
+                actionResult = await userController.GetUser(user.Id);
+                okObjectResult = Assert.IsType<OkObjectResult>(actionResult);
+                user = Assert.IsType<UserViewModel>(okObjectResult.Value);
+                Assert.Equal(updatedPassword, user.Password);
+
+                actionResult = await userController.DeleteUser(user.Id);
+
+                okObjectResult = Assert.IsType<OkObjectResult>(actionResult);
+                var value = Assert.IsType<bool>(okObjectResult.Value);
+                Assert.True(value);
+            }
+        }
+
+        private IServiceCollection BuildDependencies()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddScoped<IUserRepository, UserRepositoryMock>();
+            services.AddScoped<IUnitOfWork, UnitOfWorkMock>();
+            services.AddScoped<INotificationService, NotificationHTMLService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<UserController>();
+
+            return services;
+        }
+
+    }
+}
