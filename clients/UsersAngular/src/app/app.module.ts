8945308e56899212c7eaeb16503747e22ca19714
joseantonio <joseantonio@qcompusoft.com>
2022-05-03 09:18:44 +0200

    Agregados componentes a app.module.ts.

--- clients/UsersAngular/src/app/app.module.ts
+++ clients/UsersAngular/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavmenuComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
