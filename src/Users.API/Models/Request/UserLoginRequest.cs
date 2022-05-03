8bc41ce09c7eedb9c98abf45e8d4cc4f98f590a7
joseantonio <joseantonio@qcompusoft.com>
2022-05-03 08:56:37 +0200

    Agregada validaciones a UserLoginRequest.

--- src/Users.API/Models/Request/UserLoginRequest.cs
+++ src/Users.API/Models/Request/UserLoginRequest.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Users.API.Models.Request
 {
     public class UserLoginRequest
     {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(20, ErrorMessage = "Username cannot be longer than 20 characters.")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(10, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 10 characters.")]
         public string Password { get; set; }
     }
 }
