b3959b03baea0127a6c740d2635e18b7321c4a78
joseantonio <joseantonio@qcompusoft.com>
2022-05-19 10:53:07 +0200

    Agregada comprobacion para que CoreModule no se pueda importar mas de una vez.

--- clients/UsersAngular/src/app/core/core.component.ts
+++ clients/UsersAngular/src/app/core/core.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional, SkipSelf } from '@angular/core';
+import { CoreModule } from './core.module';
 
 @Component({
   selector: 'app-core',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoreComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only.');
+    }
+  }
 
   ngOnInit(): void {
   }
