d127a5a0b34307809f12cc0d278594b6feb9b950
joseantonio <joseantonio@qcompusoft.com>
2022-05-22 18:34:42 +0200

    Agregadas las rutas de users.

--- clients/UsersAngular/src/app/app-routing.module.ts
+++ clients/UsersAngular/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { LoginComponent } from './core/components/login/login.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { HomeComponent } from './modules/home/home.component';
+import { UsersEditComponent } from './modules/users/users-edit/users-edit.component';
+import { UsersNewComponent } from './modules/users/users-new/users-new.component';
+import { UsersViewComponent } from './modules/users/users-view/users-view.component';
 import { UsersComponent } from './modules/users/users.component';
 
 const routes: Routes = [
@@ -11,6 +14,9 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent , canActivate: [AuthGuard] },
   { path: 'users', component: UsersComponent, canActivate: [AuthGuard]},
+  { path: 'users/edit/:id', component: UsersEditComponent , canActivate: [AuthGuard] },
+  { path: 'users/new', component: UsersNewComponent , canActivate: [AuthGuard] },
+  { path: 'users/view', component: UsersViewComponent , canActivate: [AuthGuard] },
   { path: '**', component: PageNotFoundComponent },
 ];
 
