eaf6861c618fd892f93fa4647fa1cd6dc82b37cf
joseantonio <joseantonio@qcompusoft.com>
2022-05-27 20:13:47 +0200

    Agregado PopupComponent a shared.

--- clients/UsersAngular/src/app/shared/shared.module.ts
+++ clients/UsersAngular/src/app/shared/shared.module.ts
@@ -2,24 +2,55 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
-import { UserService } from './services/userservice';
-import { JWTService } from './services/jwtservice';
-import { ErrorService } from './services/errorservice';
-
+import { UserService } from './services/user.service';
+import { JWTService } from './services/jwt.service';
+import { ErrorService } from './services/error.service';
+import { MatDialogClose, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PopupComponent } from './components/popup/popup.component';
+import { MaterialModule } from './material-module';
 
 @NgModule({
-  declarations: [
+  declarations: [  
+    PopupComponent
   ],
   imports: [
     CommonModule,
     SharedRoutingModule
   ],
-  exports:[    
+  exports: [
+    PopupComponent,
   ],
-  providers:[
+  providers: [
     UserService,
     JWTService,
-    ErrorService
+    ErrorService,
+    MatDialogModule,
+    MatButtonModule,    
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    SharedModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatDialogClose,
+    MatDialogModule,
   ]
 })
 export class SharedModule { }
