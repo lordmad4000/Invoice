44d908d36dcf16e76965f0f798cbecb8c54204de
José Antonio Guerrero López <lordmad4000@hotmail.com>
2022-06-04 10:04:45 +0200

    Limpiado userscomponent.

--- clients/UsersAngular/src/app/modules/users/users.component.ts
+++ clients/UsersAngular/src/app/modules/users/users.component.ts
@@ -2,12 +2,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
-
 export class UsersComponent implements OnInit {
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
