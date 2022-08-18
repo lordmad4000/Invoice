8041610bed4f24f25714fdf99e62c03bb040fce2
joseantonio <lordmad4000@hotmail.com>
2022-08-18 14:52:32 +0200

    Agregada referencia a MediatR.

--- src/Invoice.Application/DependecyInjection.cs
+++ src/Invoice.Application/DependecyInjection.cs
@@ -1,6 +1,7 @@
 using Invoice.Application.Interfaces;
 using Invoice.Application.Services;
 using Invoice.Application.Services.Configuration.Commands;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Invoice.Application
@@ -9,6 +10,7 @@ namespace Invoice.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddMediatR(typeof(DependencyInjection).Assembly);
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<ILoginService, LoginService>();
             services.AddScoped<IValidatorService, ValidatorService>();
