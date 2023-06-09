21d1da289823b6f015d82447c4d5ba798aac5ea9
joseantonio <lordmad4000@hotmail.com>
2023-06-09 19:35:04 +0200

    Refactor user. Enable nullable in domain project.

--- src/Invoice.Domain/Users/User.Aggregate.cs
+++ src/Invoice.Domain/Users/User.Aggregate.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Linq;
+using FluentValidation.Results;
 using Invoice.Domain.Base;
+using Invoice.Domain.Exceptions;
+using Invoice.Domain.Validations;
 using Invoice.Domain.ValueObjects;
 
 namespace Invoice.Domain.Users
@@ -7,23 +11,33 @@ namespace Invoice.Domain.Users
     public partial class User : IAggregateRoot
     {
         private User()
-        {
+        {            
         }
 
-        public User(EmailAddress emailAddress, string password, string firstName, string lastName)
+        public static User Create(string emailAddress, string password, string firstName, string lastName)
         {
-            Id = Guid.NewGuid();
-            CreationDate = DateTime.UtcNow;
-            Update(emailAddress, password, firstName, lastName);
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.UtcNow
+            };
+            user.Update(emailAddress, password, firstName, lastName);
+
+            return user;
         }
 
-        public void Update(EmailAddress emailAddress, string password, string firstName, string lastName)
+        public void Update(string emailAddress, string password, string firstName, string lastName)
         {
-            EmailAddress = emailAddress;
+            EmailAddress = EmailAddress.Create(emailAddress);
             Password = password;
             FirstName = firstName;
             LastName = lastName;
-        }
+
+            ValidationResult validator = new RegisterUserValidator().Validate(this);
+            if (!validator.IsValid)
+                throw new EntityValidationException(
+                    string.Join(", ", validator.Errors.Select(x => x.ErrorMessage).ToArray()));
+        }        
 
         public override string ToString()
         {
--- src/Invoice.Domain/Users/User.cs
+++ src/Invoice.Domain/Users/User.cs
@@ -6,10 +6,10 @@ namespace Invoice.Domain.Users
 {
     public partial class User : BaseEntity
     {
-        public EmailAddress EmailAddress { get; private set; }
-        public string Password { get; private set; }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
+        public EmailAddress EmailAddress { get; private set; } = new EmailAddress("defaultuser@user.com");
+        public string Password { get; private set; } = string.Empty;
+        public string FirstName { get; private set; } = string.Empty;
+        public string LastName { get; private set; } = string.Empty;
         public DateTime CreationDate { get; private set; }
     }
 }
