0c7a917a06f81dbf7a176d1e8c9b79cbb0b8779f
joseantonio <lordmad4000@hotmail.com>
2023-06-23 13:33:34 +0200

    Added round to money amount.

--- src/SimplexInvoice.Domain/ValueObjects/Money.cs
+++ src/SimplexInvoice.Domain/ValueObjects/Money.cs
@@ -3,10 +3,12 @@ using SimplexInvoice.Domain.Exceptions;
 using SimplexInvoice.Domain.ValueObjects.Validations;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace SimplexInvoice.Domain.ValueObjects;
 public class Money : ValueObject
 {
+    public const int AmountRoundDigits =  2;
     public string Currency { get; private set ; } = "USD";
     public double Amount { get; private set ; } = 0.0;
 
@@ -20,6 +22,7 @@ public class Money : ValueObject
         Amount = amount;
         Validate();
         Currency = currency.ToUpper();
+        Amount = Math.Round(amount, AmountRoundDigits);
     }
 
     private void Validate()
