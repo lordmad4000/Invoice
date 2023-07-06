63efc9b5024268e46406529f9429c5392ddb81ae
joseantonio <lordmad4000@hotmail.com>
2023-07-06 17:52:27 +0200

    Added value objects PhoneNumber and EmailAddres to create and update methods on Customer instead of basic types.

--- src/SimplexInvoice.Domain/Aggregates/Customers/Customer.Aggregate.cs
+++ src/SimplexInvoice.Domain/Aggregates/Customers/Customer.Aggregate.cs
@@ -18,11 +18,11 @@ public partial class Customer : AggregateRoot
                                   Guid idDocumentTypeId,
                                   string idDocumentNumber,
                                   Address address,
-                                  string phone,
-                                  string emailAddress)
+                                  PhoneNumber phoneNumber,
+                                  EmailAddress emailAddress)
     {
         var customer = new Customer(Guid.NewGuid());
-        customer.Update(firstName, lastName, idDocumentTypeId, idDocumentNumber, address, phone, emailAddress);
+        customer.Update(firstName, lastName, idDocumentTypeId, idDocumentNumber, address, phoneNumber, emailAddress);
 
         return customer;
     }
@@ -32,48 +32,22 @@ public partial class Customer : AggregateRoot
                        Guid idDocumentTypeId,
                        string idDocumentNumber,
                        Address address,
-                       string phone,
-                       string emailAddress)
+                       PhoneNumber phoneNumber,
+                       EmailAddress emailAddress)
     {
         FirstName = firstName;
         LastName = lastName;
         IdDocumentTypeId = idDocumentTypeId;
         IdDocumentNumber = idDocumentNumber;
         CustomerAddress = address;
-        Phone = new PhoneNumber(phone);
-        EmailAddress = new EmailAddress(emailAddress);
+        Phone = phoneNumber;
+        EmailAddress = emailAddress;
 
         ValidationResult validator = new UpdateCustomerValidator().Validate(this);
         if (!validator.IsValid)
             throw new BusinessRuleValidationException(
                 string.Join(", ", validator.Errors.Select(x => x.ErrorMessage).ToArray()));
     }
-
-    public static Customer Create(
-        string firstName,
-        string lastName,
-        Guid idDocumentTypeId,
-        string idDocumentNumber,
-        string street,
-        string city,
-        string country,
-        string postalCode,
-        string phone,
-        string emailAddress) =>
-            Create(firstName, lastName, idDocumentTypeId, idDocumentNumber, new Address(street, city, country, postalCode), phone, emailAddress);
-
-    public void Update(
-        string firstName,
-        string lastName,
-        Guid idDocumentTypeId,
-        string idDocumentNumber,
-        string street,
-        string city,
-        string country,
-        string postalCode,
-        string phone,
-        string emailAddress) =>
-            Update(firstName, lastName, idDocumentTypeId, idDocumentNumber, new Address(street, city, country, postalCode), phone, emailAddress);
     
     public override string ToString()
     {
@@ -84,6 +58,7 @@ public partial class Customer : AggregateRoot
                $"IdDocumentNumber: {IdDocumentNumber} ," +
                $"Street: {CustomerAddress.Street}, " +
                $"City: {CustomerAddress.City}, " +
+               $"State: {CustomerAddress.State}, " +
                $"Country: {CustomerAddress.Country}, " +
                $"Postal Code: {CustomerAddress.PostalCode}, " +
                $"Phone: {Phone.Phone}, " +
