a44a0e0fd0a92bf7ce382fd7407702be56997aa3
joseantonio <lordmad4000@hotmail.com>
2023-07-21 10:10:26 +0200

    Removed InvoiceTax aggregate.

--- src/SimplexInvoice.Domain/Aggregates/Invoices/InvoiceTax.cs
+++
@@ -1,8 +0,0 @@
-using SimplexInvoice.Domain.ValueObjects;
-
-namespace SimplexInvoice.Domain.Invoices;
-public partial class InvoiceTax
-{
-    public string Name { get; private set; } = string.Empty;    
-    public Money Total { get; private set; } = new Money("USD", 0.0);
-}
