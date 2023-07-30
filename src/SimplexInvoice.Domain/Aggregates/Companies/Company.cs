2f9bab7b9ae9848f50153d2da629c0c7421370fe
joseantonio <lordmad4000@hotmail.com>
2023-07-30 17:13:41 +0200

    Renamed Company property CompanyAddress to Address.

--- src/SimplexInvoice.Domain/Aggregates/Companies/Company.cs
+++ src/SimplexInvoice.Domain/Aggregates/Companies/Company.cs
@@ -1,3 +1,4 @@
+using SimplexInvoice.Domain.IdDocumentTypes;
 using SimplexInvoice.Domain.ValueObjects;
 using System;
 
@@ -7,7 +8,8 @@ public partial class Company
     public string Name { get; private set; } = string.Empty;
     public Guid IdDocumentTypeId { get; private set; } = Guid.Empty;
     public string IdDocumentNumber { get; private set; } = string.Empty;
-    public Address CompanyAddress { get; private set; } = new Address("12, Black Cat St.", "Downtown", "Los Santos", "USA", "90210");
+    public Address Address { get; private set; } = new Address("12, Black Cat St.", "Downtown", "Los Santos", "USA", "90210");
     public PhoneNumber Phone { get; private set; } = new PhoneNumber("+34 689 45 96 34");
     public EmailAddress EmailAddress { get; private set; } = new EmailAddress("defaultcompany@company.com");
+    public virtual IdDocumentType IdDocumentType { get; private set; } = IdDocumentType.Create("CIF");
 }
