751c2bf1d235cddc689328fb870936d042b604da
joseantonio <lordmad4000@hotmail.com>
2023-09-20 15:08:34 +0200

    Added GetByIdUrl method to ApiController.

--- src/SimplexInvoice.Api/Controllers/ApiController.cs
+++ src/SimplexInvoice.Api/Controllers/ApiController.cs
@@ -1,8 +1,21 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SimplexInvoice.Api.Controllers
 {
-    public class ApiController : ControllerBase
+    public abstract class ApiController : ControllerBase
     {        
+        protected string GetByIdUrl(Guid id)
+        {
+            string url = $"UNKNOWN_URL/{id}";
+            if (this.Request is not null)
+            {
+                string path = this.Request.Path.ToString();
+                var baseUrl = path.Substring(0, path.LastIndexOf("/"));
+                url = $"{this.Request.Scheme}://{this.Request.Host}{baseUrl}/GetById{id}";
+            }
+
+            return url;
+        }
     }
 }
