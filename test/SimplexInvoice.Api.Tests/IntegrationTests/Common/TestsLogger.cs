839b3c6b5a75f11ab04411c8133d06a74e4ac392
joseantonio <lordmad4000@hotmail.com>
2023-09-20 15:15:41 +0200

    Added CustomLogger TestsLogger to Api tests.

---
+++ test/SimplexInvoice.Api.Tests/IntegrationTests/Common/TestsLogger.cs
@@ -0,0 +1,67 @@
+﻿using SimplexInvoice.Application.Common.Interfaces.Persistance;
+
+namespace SimplexInvoice.Api.Tests.IntegrationTests.Common
+{
+    internal class TestsLogger : ICustomLogger
+    {
+        public void Debug(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Debug<T>(string message, T propertyValue)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Error<T>(string message, T propertyValue)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Fatal(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Fatal<T>(string message, T propertyValue)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Information(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Information<T>(string message, T propertyValue)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Verbose(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Verbose<T>(string message, T propertyValue)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Warning(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Warning<T>(string message, T propertyValue)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
