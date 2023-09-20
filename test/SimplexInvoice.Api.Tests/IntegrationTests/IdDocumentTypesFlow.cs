350344e34a062682cece9f517c6465bf61de8374
joseantonio <lordmad4000@hotmail.com>
2023-09-20 15:16:20 +0200

    Added RegisterModifyRemove test to IdDocumentTypesFlow.

---
+++ test/SimplexInvoice.Api.Tests/IntegrationTests/IdDocumentTypesFlow.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using SimplexInvoice.Api.Controllers;
+using SimplexInvoice.Api.Models.Request;
+using SimplexInvoice.Application.Common.Dto;
+using SimplexInvoice.SimplexInvoice.Api;
+
+namespace SimplexInvoice.Api.Tests.IntegrationTests;
+
+public class IdDocumentTypesFlow
+{
+    [Fact]
+#pragma warning disable CS8602
+    public async Task RegisterIdDocumentType_Then_ModifyIt_Then_RemoveIt()
+    {
+        //Arrange
+        var idDocumentTypeDto = new IdDocumentTypeDto { Name = "TEST" };
+        IServiceCollection services = ServicesConfiguration.BuildDependencies();
+        using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+        {
+            var idDocumentTypesController = serviceProvider.GetRequiredService<IdDocumentTypesController>();
+            var idDocumentTypeRegisterRequest = new IdDocumentTypeRegisterRequest { Name = "TEST" };
+
+            //Act
+            IActionResult actionResult = await idDocumentTypesController.Register(idDocumentTypeRegisterRequest, new CancellationToken());
+
+            //Assert
+            var createdResult = actionResult as CreatedResult;
+            Assert.NotNull(createdResult);
+
+            idDocumentTypeDto = createdResult?.Value as IdDocumentTypeDto;
+            Assert.NotNull(idDocumentTypeDto);
+        };
+
+        //Arrange
+        services = ServicesConfiguration.BuildDependencies();
+        using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+        {
+            var idDocumentTypesController = serviceProvider.GetRequiredService<IdDocumentTypesController>();
+            idDocumentTypeDto.Name = "TEST MOD";
+
+            //Act
+            IActionResult actionResult = await idDocumentTypesController.Update(idDocumentTypeDto, new CancellationToken());
+
+            //Assert
+            var okResult = actionResult as OkObjectResult;
+            Assert.NotNull(okResult);
+            Assert.NotNull(okResult.Value);
+
+            //Act
+            actionResult = await idDocumentTypesController.Delete(idDocumentTypeDto?.Id ?? Guid.Empty, new CancellationToken());
+
+            //Assert
+            okResult = actionResult as OkObjectResult;
+            bool result = Assert.IsType<bool>(okResult.Value);
+            Assert.True(result);
+        }
+#pragma warning restore CS8602
+
+    }
+
+}
