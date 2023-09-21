915f16272c3eac0f8e8e7df78bd749a06c2825bf
joseantonio <lordmad4000@hotmail.com>
2023-09-21 16:18:40 +0200

    Added CustomConvert to cast Results.

---
+++ test/SimplexInvoice.Api.Tests/IntegrationTests/Common/CustomConvert.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimplexInvoice.Api.Controllers;
+using SimplexInvoice.Api.Models.Request;
+using SimplexInvoice.Application.Common.Dto;
+using SimplexInvoice.Application.Common.Interfaces.Persistance;
+using System.ComponentModel.Design.Serialization;
+
+namespace SimplexInvoice.Api.Tests.IntegrationTests.Common
+{
+    internal static class CustomConvert
+    {
+        public static T? CreatedResultTo<T>(IActionResult actionResult)
+        {
+            var createdResult = actionResult as CreatedResult;
+            if (createdResult is null || createdResult.Value is null)
+                return default(T);
+
+            return (T)createdResult.Value;
+        }
+
+        public static T? OkResultTo<T>(IActionResult actionResult)
+        {            
+            var okResult = actionResult as OkObjectResult;
+            if (okResult is null || okResult.Value is null)
+                return default(T);
+
+            return (T)okResult.Value;
+        }
+
+    }
+}
