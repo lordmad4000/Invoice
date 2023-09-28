b4caac5b80ac22d1994d393f4dace78e3faa5539
joseantonio <lordmad4000@hotmail.com>
2023-09-28 11:48:53 +0200

    Added IdDocumentTypes services and models.

---
+++ src/SimplexInvoice.Api/ClientApp/src/app/shared/models/iddocumenttypedto.ts
@@ -0,0 +1,4 @@
+export class IdDocumentTypeDto{
+    id = '';
+    name = '';
+}
--- src/SimplexInvoice.Api/ClientApp/src/app/shared/services/index.ts
+++ src/SimplexInvoice.Api/ClientApp/src/app/shared/services/index.ts
@@ -1,5 +1,6 @@
 export * from './authentication.service';
 export * from './error.service';
+export * from './iddocumenttypes.service';
 export * from './jwt.service';
 export * from './loadfile.service';
 export * from './popup.service';
