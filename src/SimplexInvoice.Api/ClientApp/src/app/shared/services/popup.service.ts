efec56ecf8508410cdc6e1dc75599917afbdd81d
joseantonio <lordmad4000@hotmail.com>
2023-10-02 21:25:38 +0200

    Added translation to Popup Service.

--- src/SimplexInvoice.Api/ClientApp/src/app/shared/services/popup.service.ts
+++ src/SimplexInvoice.Api/ClientApp/src/app/shared/services/popup.service.ts
@@ -1,18 +1,22 @@
-import { Injectable, NgZone } from "@angular/core";
-import { MatDialog, MatDialogRef } from "@angular/material/dialog";
+import { CustomTranslateService } from "./customtranslate.service";
 import { GlobalConstants } from "src/app/shared/const/global-constants";
-import { PopupProperties } from "../models/popup-properties";
-import { PopupComponent } from "src/app/components";
+import { Injectable } from "@angular/core";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { PopupButton } from "../models/popup-button";
+import { PopupComponent } from "src/app/components";
+import { PopupProperties } from "../models/popup-properties";
 
 @Injectable({
     providedIn: "root"
 })
 export class PopupService {
 
-    public result: any;
+    private translate: any = (key: string) =>
+        this.translateService.instant('popup.' + key);
 
-    constructor(public dialog: MatDialog, private zone: NgZone) { }
+    public result: any;
+    constructor(public dialog: MatDialog,
+        private translateService: CustomTranslateService) { }
 
     public openCustomDialog(popupProperties: PopupProperties): MatDialogRef<any> {
         const dialogRef = this.dialog.open(PopupComponent, {
@@ -24,11 +28,11 @@ export class PopupService {
         return dialogRef;
     }
 
-    public createConfirmPopup(title: string,msg: string): MatDialogRef<any> {
+    public createConfirmPopup(title: string, msg: string): MatDialogRef<any> {
         const popConfirm = new PopupProperties(title, msg, "25rem", "auto");
 
-        const buttonNo = new PopupButton("NO", GlobalConstants.popupNoValue);
-        const buttonYes = new PopupButton("YES", GlobalConstants.popupYesValue);
+        const buttonNo = new PopupButton(this.translate("button_no"), GlobalConstants.popupNoValue);
+        const buttonYes = new PopupButton(this.translate("button_yes"), GlobalConstants.popupYesValue);
         popConfirm.actions = [];
 
         popConfirm.actions.push(buttonYes);
@@ -40,18 +44,18 @@ export class PopupService {
     public openPopupAceptar(title: string, msg: string, width: string, height: string) {
         const popupSettings = new PopupProperties(title, msg, width, height);
 
-        const buttonAceptar = new PopupButton("OK", GlobalConstants.popupConfirmValue);
+        const buttonAceptar = new PopupButton(this.translate("button_ok"), GlobalConstants.popupConfirmValue);
         popupSettings.actions = [];
 
         popupSettings.actions.push(buttonAceptar);
 
         return this.openCustomDialog(popupSettings);
     }
-    
-    public createInformativePopup(msg: string): MatDialogRef<any> {
-        const popInfo = new PopupProperties("POPUP.INFORMACION", msg, "31.25rem", "auto");
 
-        const buttonOk = new PopupButton("OK", GlobalConstants.popupConfirmValue);
+    public createInformativePopup(msg: string): MatDialogRef<any> {
+        const popInfo = new PopupProperties(this.translate("button_info"), msg, "31.25rem", "auto");
+
+        const buttonOk = new PopupButton(this.translate("button_ok"), GlobalConstants.popupConfirmValue);
         popInfo.actions = [];
 
         popInfo.actions.push(buttonOk);
