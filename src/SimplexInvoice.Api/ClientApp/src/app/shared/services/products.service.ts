f77ff561d2dd97fd10bafaa0a6cbeed2ec684bdf
joseantonio <lordmad4000@hotmail.com>
2023-10-18 16:13:40 +0200

    Added Products Service and Models.

---
+++ src/SimplexInvoice.Api/ClientApp/src/app/shared/services/products.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { ProductDto } from '../models/productdto';
+import { ProductRegisterRequest } from '../models/productregisterrequest';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class ProductsService {
+
+    private baseUrl = `${environment.API_BASE_URL}/api/Products`;
+
+    constructor(protected httpClient: HttpClient) {
+    }
+
+    public GetAll(): Observable<Array<ProductDto>> {
+
+        const url = `${this.baseUrl}/GetAll`;
+
+        return this.httpClient.get<Array<ProductDto>>(url);
+    }
+
+    public Get(id: string): Observable<ProductDto> {
+
+        const url = `${this.baseUrl}/GetById${encodeURIComponent(String(id))}`;
+
+        return this.httpClient.get<ProductDto>(url);
+    }
+
+    public Post(productRegisterRequest: ProductRegisterRequest): Observable<ProductDto> {
+
+        const url = `${this.baseUrl}/Register`;
+
+        return this.httpClient.post<ProductDto>(url, productRegisterRequest);
+    }
+
+    public Update(productDto: ProductDto): Observable<ProductDto> {
+
+        const url = `${this.baseUrl}/Update`;
+
+        return this.httpClient.put<ProductDto>(url, productDto);
+    }
+
+    public Delete(id: string): Observable<boolean> {
+
+        const url = `${this.baseUrl}/Delete/${encodeURIComponent(String(id))}`;
+
+        return this.httpClient.delete<boolean>(url);
+    }
+
+}
