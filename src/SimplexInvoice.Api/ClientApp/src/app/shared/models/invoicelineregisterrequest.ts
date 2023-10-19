277f0b590b0bde07c0fc7788448740ea9a6d8909
joseantonio <lordmad4000@hotmail.com>
2023-10-19 10:31:36 +0200

    Added Frontend InvoiceLine models.

---
+++ src/SimplexInvoice.Api/ClientApp/src/app/shared/models/invoicelineregisterrequest.ts
@@ -0,0 +1,13 @@
+export class InvoiceLineRegisterRequest {
+    lineNumber: string = '';
+    productCode: string = '';
+    productName: string = '';
+    productDescription: string = '';
+    packages: number = 0;
+    quantity: number = 0;
+    price: number = 0;
+    currency: string = '';
+    taxName: string = '';
+    taxRate: number = 0;
+    discountRate: number = 0;
+}
