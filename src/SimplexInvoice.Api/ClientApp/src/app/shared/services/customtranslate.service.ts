cb6b53c98b09809fec01df903ff60ed0ccec528e
joseantonio <lordmad4000@hotmail.com>
2023-10-23 14:52:51 +0200

    Added Currency List.

--- src/SimplexInvoice.Api/ClientApp/src/app/shared/services/customtranslate.service.ts
+++ src/SimplexInvoice.Api/ClientApp/src/app/shared/services/customtranslate.service.ts
@@ -1,19 +1,26 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Language } from '../models/language';
 import { Observable } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
+import { CurrencyInfo } from '../models/currencyinfo';
 
 @Injectable({
     providedIn: 'root',
 })
 export class CustomTranslateService {
 
-    private _currentLanguage: string;
     public supportedLanguages: Language[] = [
         { value: 'en-US', display: 'English' },
         { value: 'es-ES', display: 'Español' },
     ]
 
+    private _currencyList: CurrencyInfo[] = new Array<CurrencyInfo>();
+    public get currencyList(): CurrencyInfo[] {
+        return this._currencyList;
+    }
+
+    private _currentLanguage: string;
     public get currentLanguage() {
         return this._currentLanguage;
     }
@@ -23,14 +30,33 @@ export class CustomTranslateService {
         this.translateService.use(value);
     }
 
-    constructor(private translateService: TranslateService) {
+    constructor(private translateService: TranslateService, private httpClient: HttpClient) {
+        this.loadCurrencyList();
         this._currentLanguage = this.supportedLanguages[0].value;
         this.translateService.addLangs(this.supportedLanguages.map(c => c.value));
         let language = this.translateService.getBrowserLang();
         language = this.supportedLanguages.map(c => c.value)
-                                          .find(c => c === language);
+            .find(c => c === language);
         if (language !== undefined)
-            this._currentLanguage = language;        
+            this._currentLanguage = language;
+
+        this.use();
+    }
+
+    private loadCurrencyList() {
+        const url = '../../../assets/currency_list.json';
+        this.httpClient.get(url).subscribe({
+            next: (res: any) => {
+                if (res) {
+                    this._currencyList = res;
+                }
+            },
+            error: (err: HttpErrorResponse) => {
+                this._currencyList = new Array<CurrencyInfo>();
+                this._currencyList.push(new CurrencyInfo());
+                console.log(err.message)
+            }
+        })
     }
 
     public use(): void {
