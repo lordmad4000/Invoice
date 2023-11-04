8493c72d85cb5f6c41dd7237750dd45b4e8b0acd
joseantonio <lordmad4000@hotmail.com>
2023-11-04 20:34:08 +0100

    Added Get BasicCustomers Contains IdDocumentNumber Query and Handler.

---
+++ src/SimplexInvoice.Application/UseCases/Customers/Queries/GetBasicCustomersContainsIdDocumentNumber/GetBasicCustomersContainsIdDocumentNumberQueryHandler.cs
@@ -0,0 +1,29 @@
+using MediatR;
+using SimplexInvoice.Application.Common.Interfaces.Persistance;
+using SimplexInvoice.Application.Common.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimplexInvoice.Application.Customers.Queries;
+public class GetBasicCustomersContainsIdDocumentNumberQueryHandler : IRequestHandler<GetBasicCustomersContainsIdDocumentNumberQuery, ICollection<BasicCustomer>>
+{
+    private readonly ICustomerRepository _customerRepository;
+    private readonly ICustomLogger _logger;
+
+    public GetBasicCustomersContainsIdDocumentNumberQueryHandler(ICustomerRepository customerRepository,
+                                                           ICustomLogger logger)
+    {
+        _customerRepository = customerRepository;
+        _logger = logger;
+    }
+
+    public async Task<ICollection<BasicCustomer>> Handle(GetBasicCustomersContainsIdDocumentNumberQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<BasicCustomer> basicCustomers = await _customerRepository.GetBasicCustomerContainsIdDocumentNumber(request.IdDocumentNumber, cancellationToken);
+        _logger.Debug($"GetBasicCustomers count: {basicCustomers.Count()}");
+
+        return basicCustomers.ToList();
+    }
+}
