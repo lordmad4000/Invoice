058fec9db9408287a0b4dc4e8d28448e874ef24a
joseantonio <lordmad4000@hotmail.com>
2023-11-04 20:32:16 +0100

    Added Get BasicCustomer Contains FullName, Email and IdDocumentNumber Queries to CustomerRepository.

--- src/SimplexInvoice.Infra/Repositories/CustomerRepository.cs
+++ src/SimplexInvoice.Infra/Repositories/CustomerRepository.cs
@@ -1,6 +1,14 @@
-﻿using SimplexInvoice.Application.Common.Interfaces.Persistance;
+﻿using Microsoft.EntityFrameworkCore;
+using SimplexInvoice.Application.Common.Interfaces.Persistance;
+using SimplexInvoice.Application.Common.Models;
 using SimplexInvoice.Domain.Customers;
 using SimplexInvoice.Infra.Data;
+using SimplexInvoice.Infra.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SimplexInvoice.Infra.Repositories
 {
@@ -12,5 +20,103 @@ namespace SimplexInvoice.Infra.Repositories
             _context = unitOfWork.GetContext();
         }
 
+        public async Task<IEnumerable<BasicCustomer>> GetBasicCustomerContainsFullName(string fullName, CancellationToken cancellationToken, bool tracking = false)
+        {
+            var cacheKey = $"{_cacheKey}-GetBasicCustomerContainsFullName-{fullName}";
+
+            if (!TryGetCache(cacheKey, out List<BasicCustomer> results))
+            {
+                try
+                {
+                    var query = _context.Customer.AsQueryable();
+                    if (!tracking)
+                        query = query.AsNoTracking();
+
+                    results = await query.Where(c => c.FirstName.ToLower().Contains(fullName.ToLower()) || c.LastName.ToLower().Contains(fullName.ToLower()))
+                                         .Select(c => new BasicCustomer
+                                         {
+                                             Id = c.Id,
+                                             FullName = $"{c.FirstName} {c.LastName}",
+                                             IdDocumentNumber = c.IdDocumentNumber,
+                                             Phone = c.Phone.Phone,
+                                             Email = c.EmailAddress.Address,
+                                         })
+                                         .ToListAsync(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    throw new DataBaseException(ex.InnerException.Message);
+                }
+                TrySetCache(cacheKey, results);
+            }
+
+            return results;
+        }
+
+        public async Task<IEnumerable<BasicCustomer>> GetBasicCustomerContainsEmail(string email, CancellationToken cancellationToken, bool tracking = false)
+        {
+            var cacheKey = $"{_cacheKey}-GetBasicCustomerContainsEmail-{email}";
+
+            if (!TryGetCache(cacheKey, out List<BasicCustomer> results))
+            {
+                try
+                {
+                    var query = _context.Customer.AsQueryable();
+                    if (!tracking)
+                        query = query.AsNoTracking();
+
+                    results = await query.Where(c => c.EmailAddress.Address.ToLower().Contains(email.ToLower()))
+                                         .Select(c => new BasicCustomer
+                                         {
+                                             Id = c.Id,
+                                             FullName = $"{c.FirstName} {c.LastName}",
+                                             IdDocumentNumber = c.IdDocumentNumber,
+                                             Phone = c.Phone.Phone,
+                                             Email = c.EmailAddress.Address,
+                                         })
+                                         .ToListAsync(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    throw new DataBaseException(ex.InnerException.Message);
+                }
+                TrySetCache(cacheKey, results);
+            }
+
+            return results;
+        }
+
+        public async Task<IEnumerable<BasicCustomer>> GetBasicCustomerContainsIdDocumentNumber(string idDocumentNumber, CancellationToken cancellationToken, bool tracking = false)
+        {
+            var cacheKey = $"{_cacheKey}-GetBasicCustomerContainsIdDocumentNumber-{idDocumentNumber}";
+
+            if (!TryGetCache(cacheKey, out List<BasicCustomer> results))
+            {
+                try
+                {
+                    var query = _context.Customer.AsQueryable();
+                    if (!tracking)
+                        query = query.AsNoTracking();
+
+                    results = await query.Where(c => c.IdDocumentNumber.ToLower().StartsWith(idDocumentNumber.ToLower()))
+                                         .Select(c => new BasicCustomer
+                                         {
+                                             Id = c.Id,
+                                             FullName = $"{c.FirstName} {c.LastName}",
+                                             IdDocumentNumber = c.IdDocumentNumber,
+                                             Phone = c.Phone.Phone,
+                                             Email = c.EmailAddress.Address,
+                                         })
+                                         .ToListAsync(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    throw new DataBaseException(ex.InnerException.Message);
+                }
+                TrySetCache(cacheKey, results);
+            }
+
+            return results;
+        }
     }
 }
