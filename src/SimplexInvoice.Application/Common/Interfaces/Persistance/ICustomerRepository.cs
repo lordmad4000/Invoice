091bfa5cf387a4a05061bfcb4a47ebc089ae15e6
joseantonio <lordmad4000@hotmail.com>
2023-11-04 20:32:41 +0100

    Added Get BasicCustomer Contains FullName, Email and IdDocumentNumber Queries to ICustomerRepository.

--- src/SimplexInvoice.Application/Common/Interfaces/Persistance/ICustomerRepository.cs
+++ src/SimplexInvoice.Application/Common/Interfaces/Persistance/ICustomerRepository.cs
@@ -1,8 +1,15 @@
-﻿using SimplexInvoice.Domain.Customers;
+﻿using SimplexInvoice.Application.Common.Models;
+using SimplexInvoice.Domain.Customers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace SimplexInvoice.Application.Common.Interfaces.Persistance
 {
     public interface ICustomerRepository : IAsyncRepository<Customer>
     {
+        Task<IEnumerable<BasicCustomer>> GetBasicCustomerContainsFullName(string fullName, CancellationToken cancellationToken, bool tracking = false);
+        Task<IEnumerable<BasicCustomer>> GetBasicCustomerContainsEmail(string email, CancellationToken cancellationToken, bool tracking = false);
+        Task<IEnumerable<BasicCustomer>> GetBasicCustomerContainsIdDocumentNumber(string idDocumentNumber, CancellationToken cancellationToken, bool tracking = false);
     }
 }
