dcbdd106e4ba8e82c6368ad768fb097d40c31ba0
joseantonio <lordmad4000@hotmail.com>
2023-11-06 16:58:46 +0100

    Added Get BasicProducts Contains Name Queries to ProductRepository.

--- src/SimplexInvoice.Application/Common/Interfaces/Persistance/IProductRepository.cs
+++ src/SimplexInvoice.Application/Common/Interfaces/Persistance/IProductRepository.cs
@@ -1,8 +1,13 @@
-﻿using SimplexInvoice.Domain.Products;
+﻿using SimplexInvoice.Application.Common.Models;
+using SimplexInvoice.Domain.Products;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace SimplexInvoice.Application.Common.Interfaces.Persistance
 {
     public interface IProductRepository : IAsyncRepository<Product>
     {
+        Task<IEnumerable<BasicProduct>> GetBasicProductsContainsName(string name, CancellationToken cancellationToken, bool tracking = false);
     }
 }
