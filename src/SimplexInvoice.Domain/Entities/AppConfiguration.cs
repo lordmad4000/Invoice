0d6bd4899db7456cad9007aff602ddffc039d2b8
joseantonio <lordmad4000@hotmail.com>
2023-11-18 20:39:51 +0100

    Added AppConfiguration Domain Entity.

---
+++ src/SimplexInvoice.Domain/Entities/AppConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SimplexInvoice.Domain.Entities
+{
+    public class AppConfiguration
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string LastInvoiceNumber { get; set; } = "0000000000";
+    }
+}
