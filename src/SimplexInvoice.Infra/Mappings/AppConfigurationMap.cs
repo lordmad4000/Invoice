9a11781b9c4d06fcdddb84b68d7c708c5b27bbf4
joseantonio <lordmad4000@hotmail.com>
2023-11-19 15:05:22 +0100

    Added SeedData to AppConfiguration.

--- src/SimplexInvoice.Infra/Mappings/AppConfigurationMap.cs
+++ src/SimplexInvoice.Infra/Mappings/AppConfigurationMap.cs
@@ -2,18 +2,25 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
 using SimplexInvoice.Domain.Entities;
 
-namespace SimplexInvoice.Infra.Mappings
+namespace SimplexInvoice.Infra.Mappings;
+public class AppConfigurationMap : IEntityTypeConfiguration<AppConfiguration>
 {
-    public class AppConfigurationMap : IEntityTypeConfiguration<AppConfiguration>
+    public void Configure(EntityTypeBuilder<AppConfiguration> builder)
     {
-        public void Configure(EntityTypeBuilder<AppConfiguration> builder)
-        {
-            builder.HasIndex(c => c.Id)
-                   .IsUnique();
+        builder.HasIndex(c => c.Id)
+               .IsUnique();
 
-            builder.Property(c => c.LastInvoiceNumber)
-                   .IsRequired();
-        }
+        builder.Property(c => c.LastInvoiceNumber)
+               .IsRequired();
 
+        builder.HasData(SeedData());
     }
+    
+    private AppConfiguration SeedData()
+    {
+        var appConfiguration = new AppConfiguration();
+
+        return appConfiguration;
+    }
+
 }
