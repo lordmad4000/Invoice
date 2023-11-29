14c5b199cea827e8837306380a63b0949a4a7587
joseantonio <lordmad4000@hotmail.com>
2023-11-29 12:00:11 +0100

    Fix TaxRate Application Query Handlers.

--- src/SimplexInvoice.Application/UseCases/TaxRates/Commands/Remove/TaxRateRemoveHandler.cs
+++ src/SimplexInvoice.Application/UseCases/TaxRates/Commands/Remove/TaxRateRemoveHandler.cs
@@ -8,12 +8,15 @@ namespace SimplexInvoice.Application.TaxRates.Commands;
 
 public class TaxRateRemoveHandler : IRequestHandler<TaxRateRemoveCommand, bool>
 {
+    private readonly IUnitOfWork _unitOfWork;
     private readonly ITaxRateRepository _taxRateRepository;
     private readonly ICustomLogger _logger;
 
-    public TaxRateRemoveHandler(ITaxRateRepository taxRateRepository,
+    public TaxRateRemoveHandler(IUnitOfWork unitOfWork,
+                                ITaxRateRepository taxRateRepository,
                                 ICustomLogger logger)
     {
+        _unitOfWork = unitOfWork;
         _taxRateRepository = taxRateRepository;
         _logger = logger;
     }
@@ -21,8 +24,8 @@ public class TaxRateRemoveHandler : IRequestHandler<TaxRateRemoveCommand, bool>
     public async Task<bool> Handle(TaxRateRemoveCommand request, CancellationToken cancellationToken)
     {
         await _taxRateRepository.DeleteAsync(request.Id, cancellationToken);
-        //if (await _taxRateRepository.SaveChangesAsync(cancellationToken) == 0)
-        //    throw new TaxRateRemovingException($"Error removing the TaxRate.");
+        if (await _unitOfWork.SaveChangesAsync(cancellationToken) == 0)
+            throw new TaxRateRemovingException($"Error removing the TaxRate.");
 
         _logger.Debug(@$"TaxRate with id {request.Id} removed.");
 
--- src/SimplexInvoice.Application/UseCases/TaxRates/Commands/Update/TaxRateUpdateHandler.cs
+++ src/SimplexInvoice.Application/UseCases/TaxRates/Commands/Update/TaxRateUpdateHandler.cs
@@ -2,7 +2,6 @@ using AutoMapper;
 using MediatR;
 using SimplexInvoice.Application.Common.Dto;
 using SimplexInvoice.Application.Common.Interfaces.Persistance;
-using SimplexInvoice.Application.Interfaces;
 using SimplexInvoice.Application.TaxRates.Exceptions;
 using SimplexInvoice.Application.Common.Exceptions;
 using System.Threading;
@@ -11,14 +10,17 @@ using System.Threading.Tasks;
 namespace SimplexInvoice.Application.TaxRates.Commands;
 public class TaxRateUpdateHandler : IRequestHandler<TaxRateUpdateCommand, TaxRateDto>
 {
+    private readonly IUnitOfWork _unitOfWork;
     private readonly ITaxRateRepository _taxRateRepository;
     private readonly IMapper _mapper;
     private readonly ICustomLogger _logger;
 
-    public TaxRateUpdateHandler(ITaxRateRepository taxRateRepository,
+    public TaxRateUpdateHandler(IUnitOfWork unitOfWork,
+                                ITaxRateRepository taxRateRepository,
                                 IMapper mapper,
                                 ICustomLogger logger)
     {
+        _unitOfWork = unitOfWork;
         _taxRateRepository = taxRateRepository;
         _mapper = mapper;
         _logger = logger;
@@ -31,8 +33,8 @@ public class TaxRateUpdateHandler : IRequestHandler<TaxRateUpdateCommand, TaxRat
 
         taxRate.Update(request.Name, request.Value);
         TaxRateDto taxRateDto = _mapper.Map<TaxRateDto>(await _taxRateRepository.UpdateAsync(taxRate, cancellationToken));
-        //if (await _taxRateRepository.SaveChangesAsync(cancellationToken) == 0)
-        //    throw new TaxRateUpdatingException($"Error updating the TaxRate.");
+        if (await _unitOfWork.SaveChangesAsync(cancellationToken) == 0)
+            throw new TaxRateUpdatingException($"Error updating the TaxRate.");
 
         _logger.Debug(@$"TaxRate Updated successfully with data: {taxRate}");
 
