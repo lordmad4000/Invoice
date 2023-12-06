89a1f803aed81565b00969ebee2735c1bfef504d
joseantonio <lordmad4000@hotmail.com>
2023-12-06 19:00:23 +0100

    Added Extensions Methods ToDateOnly and ToDateTime.

---
+++ src/SimplexInvoice.Application/Extensions/DateTimeExtends.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimplexInvoice.Application.Extensions
+{
+    public static class DateTimeExtends
+    {
+        public static DateOnly? ToDateOnly(this DateTime? date)
+        {
+            if (date is null)
+                return null;
+
+            return new DateOnly(date.Value.Year, date.Value.Month, date.Value.Day);
+        }
+
+        public static DateTime? ToDateTime(this DateOnly? date)
+        {
+            if (date is null)
+                return null;
+
+            return new DateTime(date.Value.Year, date.Value.Month, date.Value.Day);
+        }
+    }
+}
